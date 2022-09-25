ac5ef5f571c63dfbba8cfabcfdbdbc5f445836d1
V Mendez <victormendez990@gmail.com>
2022-09-25 16:50:49 -0600

    Add comments/sudo code to JS

---
+++ develop/assets/js/script.js
@@ -0,0 +1,19 @@
+//Variables to be defined
+
+//Init function
+
+//Function to begin quiz when button is clicked
+
+//Function for quiz timer
+
+//Function to display question and answers to choose from
+
+//Function to check user answer chosen by user and count
+
+//Function to show quiz score at the end of timer
+
+//Function for user input prompt for Initials to save score
+
+//Function to Display Top Scores
+
+//Even Listener for start button 
