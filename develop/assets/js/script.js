acdcbd0c122f1a53bcb1060d90ba2aacc3f0a168
V Mendez <victormendez990@gmail.com>
2022-09-30 00:26:16 -0600

    Added Variables and logic to functions

--- develop/assets/js/script.js
+++ develop/assets/js/script.js
@@ -1,14 +1,15 @@
 //Variables to be defined
-var startButton = document.querySelector("");
+var startButton = document.querySelector(".start-button");
 var countDownElement = document.querySelector(".count-down");
+var mainDiv = document.querySelector(".main-div")
 
 var timer;
 var countDown;
-var correctAnswers = 0;
+var correctAnswerCount = 0;
 
 //Function to begin quiz when button is clicked, calls timer and question functions
 function startQuiz() {
-
+    countDown = 60;
     startTime();
     askQuestion();
 }
@@ -28,13 +29,19 @@ function startTime() {
 
 //Function to display question and answers to choose from and calls function to check answer
 function askQuestion() {
-
+    mainDiv.innerHTML = "<h1>"+ +"</h1>";
     checkAnswer(answerChosen);
 }
 
 //Function to check user answer chosen by user, count, and show result
 function checkAnswer() {
-    if(answerChosen )
+    if(answerChosen === correctAnswer) {
+        correctAnswerCount++;
+    }
+
+    if(answerChosen != correctAnswer) {
+        countDown - 10;
+    }
 
     if (countDown > 0){
         askQuestion();
@@ -49,7 +56,10 @@ function finalScore() {
 
 //Function for user input prompt for Initials to save score
 function saveScore() {
-
+    var initials = prompt("Enter your initials to save your score!");
+    if(initials === null) {
+        saveScore()
+    }
     highScores();
 }
 
@@ -64,7 +74,7 @@ startButton.addEventListener("click", startQuiz)
 
 //Init function, to triggers when the webpage is loaded
 function init() {
-
+    
 }
 
 //Calls init() function when page loads
